30ffbf0a3a229456a6c273591358f2ca90050be6
lucasmeloo05 <lucasmelojedi@gmail.com>
2026-02-14 16:36:26 -0300

    commit

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ ASGI_APPLICATION = "config.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "data" / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
